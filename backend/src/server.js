7a0663cb1828f9a1c7a1553208c37a513266c745
Arun Kumar <akulakumar453@gmail.com>
2026-06-24 22:27:45 +0530

    this is backend

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import User from './models/User.js';
+import authRoutes from './routes/authRoutes.js';
+import protect from './middleware/authMiddleware.js';
+
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("This is Chats API Running...");
+});
+
+app.get("/api/profile", protect, (req, res) => {
+    res.json(req.user);
+});
+
+app.use("/api/auth", authRoutes);
+
+
+
+console.log(User.modelName);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on prot ${PORT}`);
+});
